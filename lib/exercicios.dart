0b06fe4b21a1762934bfc20593d113aa9c485eb4
lucas <lucashenriquesoares6@gmail.com>
2026-02-21 18:38:38 -0300

    exercicios

--- lib/exercicios.dart
+++ lib/exercicios.dart
@@ -1,46 +1,60 @@
 // 1. Soma
 num somar(num a, num b){
-  return 0;
+  return a + b;
 }
 
 // 2. Par
 bool isPar(int n) {
+  if(n % 2 == 0) return true;
   return false;
 }
 
 // 3. Maior de idade
 bool podeEntrar(int idade){
+  if(idade >= 18) return true;
   return false;
 }
 
 // 4. Celsius para Fahrenheit
 double converter(double c){
-  return 0.0;
+  return (c * 1.8) + 32;
 }
 
 // 5. Saudação
 String saudar(String nome){
-  return "";
+  return "Olá, ${nome}!";
 }
 
 // 6. Média
 double calcularMedia(List<double> notas) {
-  return 0.0;
+  double notaTotal = 0;
+  for (double nota in notas){
+    notaTotal += nota;
+  }
+
+  return notaTotal / notas.length;
 }
 
 // 7. Filtrar Maiores
 List<int> buscarMaiores(List<int> numeros, int corte){
-  return [];
+  if (numeros.isEmpty) return [];
+  return numeros.where((numero) => numero > corte).toList();
 }
 
 // 8. Nulos (Null Safety)
 int tamanhoTexto(String? texto){
-  return 0;
+  if(texto == null || texto.isEmpty) return 0;
+  
+  return texto.length;
 }
+
 
 // 9. Carrinho com Desconto
 double fecharPedido(List<double> precos){
-  return 0.0;
+  final valorTotal = precos.reduce((a, b) => a + b);
+  
+  if (valorTotal > 500) return valorTotal - (valorTotal * 0.15)
+  return valorTotal - (valorTotal * 0.05)
 }
 
 // 10. Busca Case Insensitive
