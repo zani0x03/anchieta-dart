e67877e786847e59af092ffa92948d7e3e70fdc8
cauanMorais <cauandejesus329@gmail.com>
2026-02-22 15:47:57 -0300

    Testes resolvidos

--- lib/exercicios.dart
+++ lib/exercicios.dart
@@ -1,49 +1,93 @@
 // 1. Soma
-num somar(num a, num b){
-  return 0;
-}
+num somar(num a, num b) => a + b;
 
 // 2. Par
-bool isPar(int n) {
-  return false;
-}
+bool isPar(int n) => n % 2 == 0;
 
 // 3. Maior de idade
-bool podeEntrar(int idade){
-  return false;
-}
+bool podeEntrar(int idade) => idade >= 18;
 
 // 4. Celsius para Fahrenheit
 double converter(double c){
-  return 0.0;
+  // 1C° = 32F°
+  if (c == 0){
+    return 32;
+  }
+
+  return (c * 9/5) + 32;
 }
 
 // 5. Saudação
-String saudar(String nome){
-  return "";
-}
+String saudar(String nome) => "Olá, $nome!";
 
 // 6. Média
 double calcularMedia(List<double> notas) {
-  return 0.0;
+  double media = 0.0;
+  int nota;
+  
+  if(notas.isEmpty) {
+    return media;
+  }
+
+  for(nota = 0; nota < notas.length; nota++){
+    media += notas[nota];
+  }
+
+  return media/nota;
 }
 
 // 7. Filtrar Maiores
 List<int> buscarMaiores(List<int> numeros, int corte){
-  return [];
+  List<int> maiores = []; 
+  
+  if(numeros.isEmpty){
+    return [];
+  }
+  
+  for (int numero in numeros){
+    if (numero > corte){
+      maiores.add(numero);
+    }
+  }
+
+  return maiores;
 }
 
 // 8. Nulos (Null Safety)
-int tamanhoTexto(String? texto){
-  return 0;
-}
+int tamanhoTexto(String? texto) => texto?.length ?? 0;
 
 // 9. Carrinho com Desconto
 double fecharPedido(List<double> precos){
-  return 0.0;
+  double total = 0;
+
+  if(precos.isEmpty) {
+    return 0;
+  }
+  
+  for (double preco in precos){
+    total += preco;
+  }
+
+  if(total > 500) {
+    total = total * 0.85;
+  } else {
+    total = total * 0.95;
+  }
+
+  return total;
 }
 
 // 10. Busca Case Insensitive
 bool contemNome(List<String> nomes, String busca){
+  if(busca.isEmpty){
+    return false;
+  }
+  
+  for(String nome in nomes) {
+    if(nome.toLowerCase().contains(busca.toLowerCase())){
+      return true;
+    }
+  }
+
   return false;
 }
