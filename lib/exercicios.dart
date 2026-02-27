769e5b6905f1068ca1a94d2d1f6815fcdc4a19aa
gabriel santos <gabriel.838383@gmail.com>
2026-02-27 12:54:04 -0300

    dart gabriel

--- lib/exercicios.dart
+++ lib/exercicios.dart
@@ -1,49 +1,60 @@
 // 1. Soma
 num somar(num a, num b){
-  return 0;
+  return a + b;
 }
 
 // 2. Par
 bool isPar(int n) {
-  return false;
+  return n % 2 ==0;
 }
 
 // 3. Maior de idade
 bool podeEntrar(int idade){
-  return false;
+  return idade >= 18;
 }
 
 // 4. Celsius para Fahrenheit
 double converter(double c){
-  return 0.0;
+  return (c * 9 / 5) + 32;
 }
 
 // 5. Saudação
 String saudar(String nome){
-  return "";
+  return "oi, $nome!";
 }
 
 // 6. Média
 double calcularMedia(List<double> notas) {
-  return 0.0;
+  if (notas.isEmpty) return 0.0;
+
+  double soma = notas.reduce((a, b) => a + b);
+  return soma / notas.length;
 }
 
 // 7. Filtrar Maiores
 List<int> buscarMaiores(List<int> numeros, int corte){
-  return [];
+  return numeros.where((n) => n > corte).toList();
 }
 
 // 8. Nulos (Null Safety)
 int tamanhoTexto(String? texto){
-  return 0;
+  return texto?.length ?? 0;
 }
 
 // 9. Carrinho com Desconto
 double fecharPedido(List<double> precos){
-  return 0.0;
+double total = precos.fold(0.0, (soma, preco) => soma + preco);
+
+  if (total > 500) {
+    return total * 0.85;
+  } else {
+    return total * 0.95;
+  }
 }
 
 // 10. Busca Case Insensitive
 bool contemNome(List<String> nomes, String busca){
-  return false;
+return nomes.any(
+    (nome) => nome.toLowerCase() == busca.toLowerCase()
+  );
 }
